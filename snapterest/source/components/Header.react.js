254717ab67877618088b7b928bf7d0c89a1f9e3a
Austin Worachet <wworachet@gmail.com>
2015-12-20 14:17:48 -0800

    Header component with styling and default property

---
+++ snapterest/source/components/Header.react.js
@@ -0,0 +1,29 @@
+var React = require('react');
+
+// In React, we can defone CSS rules in a JavaScript object and pass to object as a value to React element's style property
+var headerStyle = {
+  fontSize: '16px',
+  fontWeight: '300',
+  display: 'inline-block',
+  margin: '20px 10px'
+};
+
+// Header is a stateless component that renders the h2 element
+// header text is passed from parent component via this.props.text
+// makes this component flexible and allows for reuse
+var Header = React.createClass({
+
+  getDefaultProps: function(){
+    return {
+      text: 'Default header'
+    };
+  },
+
+  render: function(){
+    return(
+      <h2 style={headerStyle}>{this.props.text}</h2>
+    );
+  }
+});
+
+module.exports = Header;
