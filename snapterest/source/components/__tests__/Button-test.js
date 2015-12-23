a8e0b55f9c72b73458a2c0771f10b6e1f345663a
Austin Worachet <wworachet@gmail.com>
2015-12-23 15:54:36 -0800

    add testing with jest

---
+++ snapterest/source/components/__tests__/Button-test.js
@@ -0,0 +1,22 @@
+jest.dontMock('../Button.react');
+
+describe('Button component', function(){
+
+  it('calls handler function on click', function(){
+
+    var React = require('react');
+    var TestUtils = require('react-addons-test-utils');
+    var Button = require('../Button.react');
+    var handleClick = jest.genMockFunction();
+
+    var buttonInstance = TestUtils.findRenderedDOMComponentWithTag(button, 'button');
+
+    TestUtils.Simulate.click(buttonInstance);
+
+    expect(handleClick).toBeCalled();
+
+    var numberOfCallsMadeIntoMockFunction = handleClick.mock.calls.length;
+
+    expect(numberOfCallsMadeIntoMockFunction).toBe(1);
+  });
+});
